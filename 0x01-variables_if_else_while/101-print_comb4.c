7e45dcbb4ee4f194cf9d2884cd2978ad663995de
Luqman <nkundimanaluqman@gmail.com>
2022-12-09 00:03:34 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -48,21 +48,11 @@ int main(void)
 																																																										{
 
 																																																																	putchar(',');
+																																																																	putchar(' ');
+																																																																			}
+																						}																													}
 
-																																																																							putchar(' ');
-
-																																																																												}
-
-																																																								}
-
-																													}
-
-																			}
-
-											}
-
-						putchar('\n');
-
-							return (0);
-
+	}			}
+putchar('\n');
+return (0);
 }		
